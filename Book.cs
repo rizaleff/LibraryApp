48b8f043a94fca8298342c2d95d6d7c0687de22a
rizaleff <rizaleffendi92@gmail.com>
2023-09-18 10:52:14 +0700

    Initial commit

---
+++ Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string PublicationYear { get; set; }
+
+        public Book (string title, string author, string publicationYear)
+        {
+            Title = title;
+            Author = author;   
+            PublicationYear = publicationYear
+        }
+
+        public void PrintBookData()
+        {
+            Console.WriteLine($"Title            : {Title}");
+            Console.WriteLine($"Author           : {Author}");
+            Console.WriteLine($"Publication Year : {PublicationYear}");
+            Console.WriteLine("-----------------------------------------")
+        }
+    }
+}
---
+++ LibraryCatalog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp
+{
+    internal class LibraryCatalog
+    {
+        public List<Book> books;
+
+        public LibraryCatalog(Book book) 
+        {
+            books = new List<Book> ();
+            books.Add (book);
+        }
+
+        public void RemoveBook(Book book)
+        {
+
+        }
+        public void FindBook(string title)
+        {
+
+        }
+
+        public void ListBook()
+        {
+            Console.WriteLine("============KATALOG BUKU===========");
+            foreach (Book book in books) 
+            {
+               book.PrintBookData();
+            }
+        }
+    }
+}
